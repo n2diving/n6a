fa6f011623213f3e048ab5e21ed32c3098e0728c
n2diving <alexis@gormley-design.com>
2016-12-08 11:49:16 -0800

    fixing review item id on user review for team ratings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -134,7 +134,7 @@ class UsersController < ApplicationController
           else
             user_review = UserReview.create(
               user_id: one_user_id,
-              review_item_id: params[:user_reviews][one_review][:review_item_id].split(">")[1].split("}")[0],
+              review_item_id: params[:user_reviews][one_review][:review_item_id],
               rate_period: params[:user_reviews][one_review][:rate_period],
               pros: params[:user_reviews][one_review][:notes],
               rating: params[:user_reviews][one_review][:rating],
