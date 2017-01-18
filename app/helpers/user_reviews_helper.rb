4b59f9c2bb76bb2824c562e692dc95d986e38519
n2diving <alexis@gormley-design.com>
2017-01-17 23:56:38 -0800

    fixing debug month additions in user review helper

--- app/helpers/user_reviews_helper.rb
+++ app/helpers/user_reviews_helper.rb
@@ -1,16 +1,14 @@
 module UserReviewsHelper
 
   def options_for_rate_period
-    this_month = Date.today.next_month.month
+    this_month = Date.today.month
 
     options = []
-    options << [Date.today.last_year.to_date.strftime("%B %Y"), Date.today]
     Date::MONTHNAMES.each_with_index do |one_month, i|
       if i <= this_month && !one_month.nil?
         options << [one_month.to_date.end_of_month.strftime("%B %Y"), one_month.to_date.end_of_month]
       end
     end
-
     options
   end
 
