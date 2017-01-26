04dca005d0d86c41136869007309ba241a6dc788
n2diving <alexis@gormley-design.com>
2017-01-26 04:46:16 -0800

    fixing potw monthly analysis

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -71,7 +71,7 @@ class UserReviewsController < ApplicationController
 
     @group_of_the_month = Team.find(team_ranking(Team.first.id, month)).team_name
     @team_of_the_month = totm.nil? ? 'No team has been selected for this month.' : totm
-    @producer_of_the_week = User.where(id: UserReview.where(rate_period: month).joins(:review_item).where(review_items: { is_team: false, is_weekly: true}).pluck(:user_id))
+    @producers_of_the_week = User.where(id: UserReview.where(rate_period: month).joins(:review_item).where(review_items: { is_team: false, is_weekly: true}).pluck(:user_id))
     @individual_by_level = individual_averages_by_role(month)
     @team_variance = {
       "average team member" => team_variance(month),
