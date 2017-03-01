c062a4014e98229d182e32b741b33066d0786774
n2diving <alexis@gormley-design.com>
2017-03-01 01:03:49 -0800

    fixing bonus totals on group performance

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -58,17 +58,18 @@ module TeamsHelper
     reviews = UserReview.where(rate_period: rate_period, team_id: team_id)
     totals = []
     reviews.pluck(:user_id).uniq.each do |one_user|
-      average = reviews.where(user_id: one_user).pluck(:rating)
+      user_reviews = reviews.where(user_id: one_user)
+      average = user_reviews.pluck(:rating)
       average.reject! {|x| x == nil}
       average.reject! {|x| x == 0}
-      bonus = bonus_totals(reviews)
+      bonus = bonus_totals(user_reviews)
 
       unless average.blank?
         totals << ((average.reduce(:+) / average.size.to_f) + bonus.sum).round(2)
       end
     end
-    puts totals
-    # totals.blank? ? 0 : ('%.2f' % (totals.reduce(:+)/totals.size.to_f))
+    # puts totals
+    totals.blank? ? 0 : ('%.2f' % (totals.reduce(:+)/totals.size.to_f))
   end
 
   def team_ranking(team_id, rate_period)
