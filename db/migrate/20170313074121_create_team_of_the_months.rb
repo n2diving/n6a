528b05cfb197eeaeea416784a300de306dbdf0f1
n2diving <alexis@gormley-design.com>
2017-03-14 01:56:39 -0700

    adding adjusted average filter on birds eye page

--- db/migrate/20170313074121_create_team_of_the_months.rb
+++ db/migrate/20170313074121_create_team_of_the_months.rb
@@ -1,10 +1,12 @@
 class CreateTeamOfTheMonths < ActiveRecord::Migration[5.0]
   def change
-    create_table :team_of_the_months do |t|
-      t.string :name
-      t.date :rate_period
+    unless ActiveRecord::Base.connection.table_exists? 'team_of_the_months'
+      create_table :team_of_the_months do |t|
+        t.string :name
+        t.date :rate_period
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
