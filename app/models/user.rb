751a8b7a7b9ab280a99f2a2d64122523fc6d47ab
n2diving <alexis@gormley-design.com>
2017-03-14 21:22:10 -0700

    fixing .5 on team show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,7 +71,7 @@ class User < ApplicationRecord
     columns.each_with_index do |one_column, i|
       user_review = UserReview.all.where(review_item_id: one_column[1], user_id: self.id, rate_period: rate_period.end_of_month, team_id: team_id).first
       @review_rows[i] = []
-      @review_rows[i] << { rating: (user_review.nil? ? '' : user_review.rating) }
+      @review_rows[i] << { rating: (user_review.nil? ? '' : (user_review.rating.nil? ? '' : ('%.1f' % user_review.rating))) }
       @review_rows[i] << { notes:(user_review.nil? ? '' : user_review.combined_notes) }
     end
     @review_rows
